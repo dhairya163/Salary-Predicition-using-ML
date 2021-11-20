fc78ad00e4e3f0479d2f4ed45f7322f54faa75a3
dhairya163 <daggarwal_be19@thapar.edu>
2021-11-20 20:03:07 +0530

    added Gradient Regressor

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import streamlit as st
-from predict_page import model_dt,model_linear
+from predict_page import model_dt,model_linear,model_gradient
 
-selection = st.sidebar.selectbox("Regression Model",("Decision Tree Regressor","Linear Regressor"))
+selection = st.sidebar.selectbox("Regression Model",("Decision Tree Regressor","Linear Regressor","Gradient Regressor"))
 
 if(selection=="Decision Tree Regressor"):
     model_dt()
 if(selection=="Linear Regressor"):
     model_linear()
+if(selection=="Gradient Regressor"):
+    model_gradient()
